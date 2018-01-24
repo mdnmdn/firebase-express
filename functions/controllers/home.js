ab6a5344a91887a1202298ff1a6d1edf797716be
mdn <marco.denittis@synesthesia.it>
2018-01-25 00:01:38 +0100

    controllers and body parser

---
+++ functions/controllers/home.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('home');
+});
+
+router.get('/info', (req, res) => {
+  res.send('info');
+});
+
+module.exports = router;
--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,34 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.root = functions.https.onRequest((request, response) => {
-  response.send("wow!");
+// based on https://codeburst.io/express-js-on-cloud-functions-for-firebase-86ed26f9144c
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+const homeController = require('./controllers/home');
+const contactController = require('./controllers/contacts');
+
+app.use('/', homeController);
+app.use('/contacts', contactController);
+
+app.get("/hi", (req, res) => {
+  res.send("i")
 });
 
-exports.wow2 = functions.https.onRequest((request, response) => {
-  response.send("wow2!");
+app.post("/post", (req, res) => {
+  console.log(req.body);
+  res.send({body: req.body});
 });
 
-exports.version =
-  functions.https.onRequest((request, response) => {
-    response.send("v: " + process.versions.node);
+app.use((req, res) => {
+  res.status(404).send("Sorry can't find that!");
+  //res.redirect('/404.html');
 });
+
+
+exports.app = functions.https.onRequest(app);
